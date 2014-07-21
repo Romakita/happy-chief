f94e5ce4414b5d6d05ce0695493fd5f8978c60d1
Romakita <rom.lenzotti@gmail.com>
2014-07-21 20:57:17 +0200

    Change DB info

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,9 +69,9 @@ module.exports = function (grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 5500,
+                port: 80,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname:   '10.61.0.16',
+                hostname:   'localhost',
                 livereload: 35729
             },
             livereload: {
