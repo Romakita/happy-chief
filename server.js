bc3035f4c7033b0ba5a14a7bd30d33964fd9a657
Romakita <rom.lenzotti@gmail.com>
2014-07-24 16:58:07 +0200

    - Change info DB - Patch recipe class.

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ var express =       require('express'),
     bodyParser =    require('body-parser');
 
 var app =  module.exports = express();
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({extended:true})); // to support URL-encoded bodies
+app.use(bodyParser.json());                         // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended:true}));    // to support URL-encoded bodies
+
+require('./lib/db').initialize(app)
+    .then(function(){
+
+        require('./lib/recipe').initialize(app);
 
 
-require('./lib/db').initialize(app);
-require('./lib/recipe').initialize(app);
+    });
+
 
